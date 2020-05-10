c5b919184c93701f6ec4d620982bbdc50cad8a35
akillika <akillika49@gmail.com>
2020-05-10 12:20:42 +0530

    Akil name

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Madhavan',
+      title: 'Akil',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Madhavan'),
+      home: MyHomePage(title: 'Akil'),
     );
   }
 }
