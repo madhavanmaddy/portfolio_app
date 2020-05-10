2e6700178dcda2df6f13ea4daf7d867d43ed7197
Madhavan Senthilkumar <58678855+madhavanselvi16092001@users.noreply.github.com>
2020-05-10 13:22:20 +0530

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Akil',
+      title: 'Portfolio',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Akil'),
+      home: MyHomePage(title: 'Portfolio'),
     );
   }
 }
