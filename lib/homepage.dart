83621e5fbcb3aefddce0db560b87ae7ad42b64b5
akillika <akillika49@gmail.com>
2020-05-10 20:12:15 +0530

    homwpage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: formpage(),
+      home: MyHomePage(),
     );
   }
 }
@@ -33,163 +33,177 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(height: 120),
-            Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: new Text(
-        "Good morning,",
-        style: TextStyle(
-          fontFamily: "Segoe UI",
-          fontWeight: FontWeight.w600,
-          fontSize: 22,
-          color: Color(0xff101010),
-        ),
-      ),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 120),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: new Text(
+              "Good morning,",
+              style: TextStyle(
+                fontFamily: "Segoe UI",
+                fontWeight: FontWeight.w600,
+                fontSize: 22,
+                color: Color(0xff101010),
+              ),
             ),
-            SizedBox(height: 10),
-            Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: new Text(
-        "What do you want us to do ?",
-        style: TextStyle(
-          fontFamily: "Segoe UI",
-          fontWeight: FontWeight.w600,
-          fontSize: 24,
-          color: Color(0xff101010),
-        ),
-      ),
-            ),
-            Image.asset('assets/images/home.png'),
-            Center(
-      child: new Container(
-        height: 116.00,
-        width: 350.00,
-        decoration: BoxDecoration(
-          color: Color(0xffffffff),
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0.00, 3.00),
-              color: Color(0xff000000).withOpacity(0.09),
-              blurRadius: 30,
-            ),
-          ],
-          borderRadius: BorderRadius.circular(33.00),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 15),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Image.asset('assets/images/flutter.png'),
-              SizedBox(width: 15,),
-              new Text(
-                "Build Flutter applications",
-                style: TextStyle(
-                  fontFamily: "Segoe UI",
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16,
-                  color: Color(0xff101010),
-                ),
-              )
-            ],
           ),
-        ),
-      ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: new Text(
+              "What do you want us to do ?",
+              style: TextStyle(
+                fontFamily: "Segoe UI",
+                fontWeight: FontWeight.w600,
+                fontSize: 24,
+                color: Color(0xff101010),
+              ),
             ),
-            SizedBox(
-      height: 15,
-            ),
-            Center(
-      child: new Container(
-        height: 116.00,
-        width: 350.00,
-        decoration: BoxDecoration(
-          color: Color(0xffffffff),
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0.00, 3.00),
-              color: Color(0xff000000).withOpacity(0.09),
-              blurRadius: 30,
-            ),
-          ],
-          borderRadius: BorderRadius.circular(33.00),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 15),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Image.asset('assets/images/ps1.png'),
-              SizedBox(width: 15,),
-              new Text(
-                "Play with designs",
-                style: TextStyle(
-                  fontFamily: "Segoe UI",
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16,
-                  color: Color(0xff101010),
-                ),
-              )
-            ],
           ),
-        ),
-      ),
-            ),
-            SizedBox(
-      height: 15,
-            ),
-            Center(
-      child: new Container(
-        height: 116.00,
-        width: 350.00,
-        decoration: BoxDecoration(
-          color: Color(0xffffffff),
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0.00, 3.00),
-              color: Color(0xff000000).withOpacity(0.09),
-              blurRadius: 30,
-            ),
-          ],
-          borderRadius: BorderRadius.circular(33.00),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 15),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              
-              Image.asset('assets/images/xd.png'),
-              SizedBox(width: 15,),
-              new Text(
-                "User experience builder",
-                style: TextStyle(
-                  fontFamily: "Segoe UI",
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16,
-                  color: Color(0xff101010),
+          Image.asset('assets/images/home.png'),
+          Center(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => formpage()),
+                );
+              },
+              child: new Container(
+                height: 116.00,
+                width: 350.00,
+                decoration: BoxDecoration(
+                  color: Color(0xffffffff),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0.00, 3.00),
+                      color: Color(0xff000000).withOpacity(0.09),
+                      blurRadius: 30,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(33.00),
                 ),
-              )
-            ],
-          ),
-        ),
-      ),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Image.asset('assets/images/flutter.png'),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      new Text(
+                        "Build Flutter applications",
+                        style: TextStyle(
+                          fontFamily: "Segoe UI",
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                          color: Color(0xff101010),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
-          ],
-        ),
-       // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Center(
+            child: new Container(
+              height: 116.00,
+              width: 350.00,
+              decoration: BoxDecoration(
+                color: Color(0xffffffff),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0.00, 3.00),
+                    color: Color(0xff000000).withOpacity(0.09),
+                    blurRadius: 30,
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(33.00),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Image.asset('assets/images/ps1.png'),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    new Text(
+                      "Play with designs",
+                      style: TextStyle(
+                        fontFamily: "Segoe UI",
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                        color: Color(0xff101010),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Center(
+            child: new Container(
+              height: 116.00,
+              width: 350.00,
+              decoration: BoxDecoration(
+                color: Color(0xffffffff),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0.00, 3.00),
+                    color: Color(0xff000000).withOpacity(0.09),
+                    blurRadius: 30,
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(33.00),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Image.asset('assets/images/xd.png'),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    new Text(
+                      "User experience builder",
+                      style: TextStyle(
+                        fontFamily: "Segoe UI",
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                        color: Color(0xff101010),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
