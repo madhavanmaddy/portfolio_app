90d74d44e46bf87b5302ddcc13c52a45c345ddfa
akillika <akillika49@gmail.com>
2020-05-17 17:15:14 +0530

    Teams page updated

--- lib/pages/teamspage.dart
+++ lib/pages/teamspage.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/services/crud.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class teamspage extends StatefulWidget {
   @override
@@ -11,11 +12,25 @@ class _teamspageState extends State<teamspage> {
   crudMethods crudObj = new crudMethods();
 
   QuerySnapshot devs;
+  double temp31;
+  int temp21;
+
   @override
   void initState() {
     crudObj.getdatadevs().then((results) {
       setState(() {
         devs = results;
+        temp21 = devs.documents.length;
+        if (temp21 % 2 == 0) {
+          temp21 = temp21 * 120;
+          temp31 = temp21.toDouble();
+        } else {
+          // temp = projects.documents.length;
+          temp21 = temp21 + 1;
+          temp21 = temp21 * 120;
+          temp31 = temp21.toDouble();
+        }
+        print('$temp31');
       });
     });
     super.initState();
@@ -26,6 +41,7 @@ class _teamspageState extends State<teamspage> {
     double sh = MediaQuery.of(context).size.height;
     double sw = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
@@ -34,13 +50,13 @@ class _teamspageState extends State<teamspage> {
               SizedBox(
                 height: 30.0,
               ),
-              Center(
-                  child: Text('Development Team',
-                      style: TextStyle(
-                        fontSize: sw*0.05,
-                      ))),
+              Text('Alone we can do so little; together we can do so much.',
+                  style: TextStyle(
+                    fontSize: sw*0.05,
+                    fontWeight: FontWeight.bold
+                  )),
               SizedBox(height: 10.0),
-              // Image.asset('assets/images/mockups.png'),
+              Image.asset('assets/images/team.png'),
               SizedBox(height: 20.0),
               devsgrid(),
             ],
@@ -52,9 +68,10 @@ class _teamspageState extends State<teamspage> {
   Widget devsgrid(){
      return devs != null
         ? SizedBox(
-            height: 750,
+            height: temp31,
             
             child: GridView.builder(
+              
 
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
@@ -100,4 +117,5 @@ class _teamspageState extends State<teamspage> {
           );
   }
   }
+  
 
